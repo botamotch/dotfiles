6c96a72ea78fd4c08ecc3bbcd081d20777b4fbd6
bmkazuya <laughing00boomer@gmail.com>
2022-02-10 13:59:30 +0900

    xprofile修正、install.sh作成

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# zsh
+ln -s $HOME/Git/dotfiles/zsh/zprofile $HOME/.zprofile
+# starship
+ln -s $HOME/Git/dotfiles/starship/starship.toml $HOME/.config/starship.toml
+# conky
+ln -s $HOME/Git/dotfiles/conky/conky.conf $HOME/.config/conky/conky.conf
+ln -s $HOME/Git/dotfiles/conky/start_conky.sh $HOME/.config/conky/start_conky.sh
+ln -s $HOME/Git/dotfiles/conky/start_conky.sh.desktop $HOME/.config/autostart/start_conky.sh.desktop
+# xprofile
+ln -s $HOME/Git/dotfiles/xprofile/xprofile $HOME/.xprofile
