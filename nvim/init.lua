4beff63348ba6c1c22237552011109f48b4d8ece
bmkazuya <laughing00boomer@gmail.com>
2022-07-05 17:45:20 +0900

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,9 +11,9 @@ vim.opt.hlsearch = true
 vim.opt.hidden = true
 vim.opt.backup = true
 vim.opt.backupdir = os.getenv("HOME") .. '/.vim/backup'
--- vim.opt.winblend = 20
--- vim.opt.pumblend = 20
--- vim.opt.termguicolors = true
+vim.opt.winblend = 20
+vim.opt.pumblend = 20
+vim.opt.termguicolors = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
@@ -28,6 +28,17 @@ vim.opt.formatoptions:remove('t')
 vim.opt.formatoptions:append('mM')
 
 vim.cmd 'autocmd TermOpen * startinsert'
+vim.cmd [[
+if executable('fcitx5')
+  let g:fcitx_state = 1
+  augroup fcitx_savestate
+    autocmd!
+    autocmd InsertLeave * let g:fcitx_state = str2nr(system('fcitx5-remote'))
+    autocmd InsertLeave * call system('fcitx5-remote -c')
+    autocmd InsertEnter * call system(g:fcitx_state == 1 ? 'fcitx5-remote -c': 'fcitx5-remote -o')
+  augroup END
+endif
+]]
 
 -- Key map =====================================================================
 vim.g.mapleader = " "
@@ -86,11 +97,11 @@ require("packer").startup(function()
 end)
 
 -- colorscheme -----------------------------------------------------------------
-vim.cmd 'autocmd ColorScheme * highlight Normal ctermbg=none'
-vim.cmd 'autocmd ColorScheme * highlight NonText ctermbg=none'
-vim.cmd 'autocmd ColorScheme * highlight LineNr ctermbg=none'
-vim.cmd 'autocmd ColorScheme * highlight Folded ctermbg=none'
-vim.cmd 'autocmd ColorScheme * highlight EndOfBuffer ctermbg=none '
+vim.cmd 'autocmd ColorScheme * highlight Normal ctermbg=none guibg=none'
+vim.cmd 'autocmd ColorScheme * highlight NonText ctermbg=none guibg=none'
+vim.cmd 'autocmd ColorScheme * highlight LineNr ctermbg=none guibg=none'
+vim.cmd 'autocmd ColorScheme * highlight Folded ctermbg=none guibg=none'
+vim.cmd 'autocmd ColorScheme * highlight EndOfBuffer ctermbg=none guibg=none'
 -- vim.cmd 'colorscheme default'
 vim.cmd 'colorscheme iceberg'
 
