060fe2d6f4c426ff86544c0dd8c96b34b0ef7110
botamotch <laughing00boomer@gmail.com>
2022-07-28 23:59:37 +0900

    zennブログ用 nvim/init_lsp.lua 修正

--- nvim/init_lsp.lua
+++ nvim/init_lsp.lua
@@ -1,11 +1,8 @@
--- packer ======================================================================
--- You must run `:PackerSync` whenever you make changes to your plugin configuration
+-- package manager
 require("packer").startup(function()
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
   use 'williamboman/mason.nvim'
-  -- use 'williamboman/nvim-lsp-installer'
-  use 'ibhagwan/fzf-lua'
 
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-path"
@@ -15,24 +12,14 @@ require("packer").startup(function()
   use "hrsh7th/vim-vsnip"
 end)
 
--- LSP Sever management
+-- 1. LSP Sever management
 require('mason-lspconfig').setup_handlers({ function(server)
   local opts = {
-    on_attach = function(client, bufnr)
-      local opts = { noremap=true, silent=true }
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    end
+    -- Function executed when the LSP server startup
+    -- on_attach = function(client, bufnr)
+    --   local opts = { noremap=true, silent=true }
+    --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    -- end,
     capabilities = require('cmp_nvim_lsp').update_capabilities(
       vim.lsp.protocol.make_client_capabilities()
     )
@@ -40,25 +27,39 @@ require('mason-lspconfig').setup_handlers({ function(server)
   require('lspconfig')[server].setup(opts)
 end })
 
--- fzf-lua
-vim.keymap.set('n', '<space>o', "<cmd>lua require('fzf-lua').files()<CR>")
-vim.keymap.set('n', '<space>f', "<cmd>lua require('fzf-lua').live_grep()<CR>")
-vim.keymap.set('n', '<space>g', "<cmd>lua require('fzf-lua').git_status()<CR>")
-vim.keymap.set('n', '<space>b', "<cmd>lua require('fzf-lua').git_branches()<CR>")
-vim.keymap.set('n', '<space>r', "<cmd>lua require('fzf-lua').lsp_references()<CR>")
-vim.keymap.set('n', '<space>d', "<cmd>lua require('fzf-lua').lsp_definitions()<CR>")
-vim.keymap.set('n', '<space>D', "<cmd>lua require('fzf-lua').lsp_decralations()<CR>")
-vim.keymap.set('n', '<space>i', "<cmd>lua require('fzf-lua').lsp_implementations()<CR>")
-vim.keymap.set('n', '<space>e', "<cmd>lua require('fzf-lua').diagnostics_document()<CR>")
+-- 2. build-in LSP function
+-- keyboard shortcut
+vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+-- LSP handlers
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+)
+-- Reference highlight
+vim.cmd [[
+highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+augroup END
+]]
 
--- 'hrsh7th/nvim-cmp'
+-- 3. completion (hrsh7th/nvim-cmp)
 local cmp = require("cmp")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
   window = {
     -- documentation = {
     --   border = "none",
@@ -101,22 +102,3 @@ cmp.setup.cmdline(":", {
     { name = "cmdline" },
   },
 })
-
-
--- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
-)
-
--- 変数ハイライト
-vim.cmd [[
-highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-
-augroup lsp_document_highlight
-  autocmd!
-  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-augroup END
-]]
