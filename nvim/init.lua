018a767e9a64e34dc25e33df363bea7b527f382b
bmkazuya <laughing00boomer@gmail.com>
2022-11-23 15:52:47 +0900

    Remove LspSaga

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,7 +93,7 @@ require("packer").startup(function()
   -- use 'dinhhuy258/git.nvim'
   use 'nvim-lualine/lualine.nvim'
   use 'windwp/nvim-autopairs'
-  use 'glepnir/lspsaga.nvim'
+  -- use 'glepnir/lspsaga.nvim'
   use {"akinsho/toggleterm.nvim", tag = '*'}
 
   -- colorscheme
@@ -226,27 +226,18 @@ require('mason-lspconfig').setup_handlers({ function(server)
       local opts = { noremap=true, silent=true }
       vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.format {async=true}<CR>', opts)
 
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, "n", 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, "n", 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-      -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "K",  "<cmd>Lspsaga hover_doc<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>Lspsaga lsp_finder<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "ga", "<cmd>Lspsaga code_action<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gn", "<cmd>Lspsaga rename<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "ge", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 
       vim.cmd [[
       let s:bl = ['json'] " set blacklist filetype
@@ -339,35 +330,35 @@ require("bufferline").setup({
   },
 })
 
-require("lspsaga").init_lsp_saga({
-  border_style = "single",
-  symbol_in_winbar = {
-    enable = false,
-  },
-  code_action_lightbulb = {
-    enable = false,
-  },
-  show_outline = {
-    win_width = 50,
-    auto_preview = false,
-    jump_key = "<CR>",
-  },
-  finder_action_keys = {
-      open = "<CR>",
-      vsplit = "s",
-      quit = "q",
-  },
-  code_action_keys = {
-      exec = "<CR>",
-      quit = "q",
-  },
-  definition_action_keys = {
-    edit = '<CR>',
-    vsplit = 's',
-    quit = 'q',
-  },
-  rename_action_quit = "<ESC>",
-})
+-- require("lspsaga").init_lsp_saga({
+--   border_style = "single",
+--   symbol_in_winbar = {
+--     enable = false,
+--   },
+--   code_action_lightbulb = {
+--     enable = false,
+--   },
+--   show_outline = {
+--     win_width = 50,
+--     auto_preview = false,
+--     jump_key = "<CR>",
+--   },
+--   finder_action_keys = {
+--       open = "<CR>",
+--       vsplit = "s",
+--       quit = "q",
+--   },
+--   code_action_keys = {
+--       exec = "<CR>",
+--       quit = "q",
+--   },
+--   definition_action_keys = {
+--     edit = '<CR>',
+--     vsplit = 's',
+--     quit = 'q',
+--   },
+--   rename_action_quit = "<ESC>",
+-- })
 vim.cmd[[
 " highlight link LspSagaLspFinderBorder FloatBorder
 " highlight link LspSagaAutoPreview FloatBorder
