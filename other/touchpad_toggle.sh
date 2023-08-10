b3139bbc1119bea7de793bf4bdf6e9d88d0185c5
Buma Kazuya <laughing00boomer@gmail.com>
2023-08-10 23:18:52 +0900

    update touchpad toggle, fcitx-reload

--- other/touchpad_toggle.sh
+++ other/touchpad_toggle.sh
@@ -5,3 +5,12 @@ val=$(xinput list-props 'VEN_04F3:00 04F3:325F Touchpad' | grep 'Device Enabled'
 xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' $((1 - $val))
 # xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' 1
 # xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' 0
+
+state="enable"
+# if $val -eq "1"; then
+#     state="enable"
+# else
+#     state="disable"
+# fi
+
+notify-send 'Touchpad Toggle' "state to $state" --icon=dialog-information
