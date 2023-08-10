663ba990c613c9d75ccd376b79929208f4327850
Kazuya Buma <laughing00boomer@gmail.com>
2023-08-11 01:01:50 +0900

    update nvim/init.lua - lspconfig setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -273,14 +273,10 @@ require("flutter-tools").setup({
   }
 })
 
+require('lspconfig').gopls.setup({})
+require('lspconfig').rust_analyzer.setup({})
 require('lspconfig').ocamllsp.setup({})
-require('lspconfig')["denols"].setup({
-  root_dir  = require('lspconfig').util.root_pattern("deno.json", "deno.jsonc"),
-})
--- if tsserver is installed
--- require('lspconfig')["tsserver"].setup({
---   root_dir  = require('lspconfig').util.root_pattern("package.json"),
--- })
+require('lspconfig').denols.setup({})
 
 local border = "single" -- single, rounded , none, shadow, double, solid
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
