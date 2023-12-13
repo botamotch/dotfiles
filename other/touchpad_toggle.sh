ee84dd1621ab2ac0e33f6032787052b8bbb2e6ed
Kazuya Buma <laughing00boomer@gmail.com>
2023-12-13 15:47:30 +0900

    update

--- other/touchpad_toggle.sh
+++ other/touchpad_toggle.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # TODO notification
-val=$(xinput list-props 'VEN_04F3:00 04F3:325F Touchpad' | grep 'Device Enabled' | awk -F : '{print $2}')
+val=$(xinput list-props 'VEN_0488:00 0488:102D Touchpad' | grep 'Device Enabled' | awk -F : '{print $2}')
 
-xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' $((1 - $val))
+xinput set-prop 'VEN_0488:00 0488:102D Touchpad' 'Device Enabled' $((1 - $val))
 # xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' 1
 # xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' 0
 
