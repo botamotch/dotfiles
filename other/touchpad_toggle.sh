203750a38565f253c954b3a085c3536fb3b7fa60
Kazuya Buma <laughing00boomer@gmail.com>
2024-10-23 10:04:49 +0900

    update touchpad_toggle.sh

--- other/touchpad_toggle.sh
+++ other/touchpad_toggle.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
-# TODO notification
-val=$(xinput list-props 'VEN_0488:00 0488:102D Touchpad' | grep 'Device Enabled' | awk -F : '{print $2}')
+#!/usr/bin/bash
 
-xinput set-prop 'VEN_0488:00 0488:102D Touchpad' 'Device Enabled' $((1 - $val))
+enabled=$(xinput list-props 'VEN_0488:00 0488:102D Touchpad' | grep 'Device Enabled' | awk -F : '{print $2}')
+
+xinput set-prop 'VEN_0488:00 0488:102D Touchpad' 'Device Enabled' $((1 - $enabled))
 # xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' 1
 # xinput set-prop 'VEN_04F3:00 04F3:325F Touchpad' 'Device Enabled' 0
 
-state="enable"
-# if $val -eq "1"; then
-#     state="enable"
-# else
-#     state="disable"
-# fi
+if [ "$enabled" -eq 0 ]; then
+    state="enable"
+else
+    state="disable"
+fi
 
-notify-send 'Touchpad Toggle' "state to $state" --icon=dialog-information
+notify-send --hint=int:transient:1 'Touchpad Toggle' "to $state" --icon=dialog-information
