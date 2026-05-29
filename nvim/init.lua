fe9ee11f19b2b26d97dff03ce41b713d1074a088
Kazuya Buma <laughing00boomer@gmail.com>
2026-05-29 13:16:26 +0900

    nvim 補完のコマンドの候補をコメントに追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -340,6 +340,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(args)
+--     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+--     -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+--     if client:supports_method('textDocument/completion') then
+--       -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+--       -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+--       -- client.server_capabilities.completionProvider.triggerCharacters = chars
+--       vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+--     end
+--   end
+-- })
+
 -- (2) Highlight Reference
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
